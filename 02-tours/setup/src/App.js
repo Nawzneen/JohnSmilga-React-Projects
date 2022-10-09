393a44af514fc023ea5889bf6ca68fa5b4662fb9
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-09 17:28:20 +0300

    enabled removing items from the list and fetching them agian

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -8,14 +8,18 @@ const url = "https://course-api.com/react-tours-project";
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [tours, setTours] = React.useState([]);
-  React.useEffect(function () {
-    setIsLoading(true);
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setTours(data));
-    setIsLoading(false);
-    console.log(tours);
-  }, []);
+  const [refresh, setRefresh] = React.useState(false);
+  React.useEffect(
+    function () {
+      setIsLoading(true);
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => setTours(data));
+      setIsLoading(false);
+      console.log(tours);
+    },
+    [refresh]
+  );
   console.log(tours);
 
   if (isLoading) {
@@ -25,9 +29,26 @@ function App() {
       </main>
     );
   }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title"> There is Nothing else to see</div>
+        <button
+          className="btn"
+          onClick={() => setRefresh((prevRefresh) => !prevRefresh)}
+        >
+          Refresh
+        </button>
+      </main>
+    );
+  }
+  function removeTour(id) {
+    setTours(tours.filter((tour) => id !== tour.id));
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
