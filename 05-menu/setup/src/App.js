002d55f2ceaa1eee24bee2ae84a759ecf55a0925
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-11 16:52:27 +0300

    Added the categories and filtering through it

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,9 +3,19 @@ import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
   const [menu, setMenu] = React.useState(items);
-  const [categories, setCategories] = React.useState([]);
+  const [categories, setCategories] = React.useState(allCategories);
+
+  function filterItems(category) {
+    console.log("isworking");
+    const newItems = items.filter((item) => item.category === category);
+    setMenu(newItems);
+    if (category === "all") {
+      setMenu(items);
+    }
+  }
   return (
     <main>
       <section className="menu section">
@@ -13,7 +23,7 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menu} />
       </section>
     </main>
