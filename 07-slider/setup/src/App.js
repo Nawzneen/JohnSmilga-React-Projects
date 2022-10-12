69d11ca8d29b94ce8fc7312a2bebec027505ed75
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-12 21:03:38 +0300

    Practicing Sliders in Reacti

--- 07-slider/setup/src/App.js
+++ 07-slider/setup/src/App.js
@@ -1,9 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
-import { FaQuoteRight } from 'react-icons/fa';
-import data from './data';
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft, FiCheckSquare } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
+import people from "./data";
 function App() {
-  return <h2>slider project setup</h2>;
+  const [index, setIndex] = useState(0);
+  const [people, setPeople] = useState(data);
+
+  function checkNum(num) {
+    if (num > data.length - 1) {
+      return (num = 0);
+    }
+    if (num < 0) {
+      return data.length - 1;
+    }
+    return num;
+  }
+  function nextPerson() {
+    setIndex((prevIndex) => {
+      let newIndex = prevIndex + 1;
+      return checkNum(newIndex);
+    });
+  }
+  function prevPerson() {
+    setIndex((prevIndex) => {
+      let newIndex = prevIndex - 1;
+      return checkNum(newIndex);
+    });
+  }
+  return (
+    <section className="section">
+      <div className="title">
+        <h2 className="title">
+          <span>/</span>Reviews
+        </h2>
+        <div className="section-center">
+          {people.map((person, peopleIndex) => {
+            const { id, image, name, title, quote } = person;
+            return (
+              <article key={id}>
+                <img src={image} alt="" className="person-img" />
+                <h4>{name}</h4>
+                <p className="text">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
+            );
+          })}
+
+          <button onClick={prevPerson} className="prev">
+            <FiChevronLeft />
+          </button>
+          <button onClick={nextPerson} className="next">
+            <FiChevronRight />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
