c07aa819c2cec285811aef79a691a68061a32936
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-14 17:50:01 +0300

    generating paragraphs on click

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -3,8 +3,42 @@ import data from "./data";
 function App() {
   const [number, setNumber] = useState(0);
   const [text, setText] = useState([]);
+  function handleSubmit(e) {
+    e.preventDefault();
+    // changing from string to number
+    let amount = parseInt(number);
+    if (number <= 0) {
+      amount = 1;
+    }
+    if (number > 8) {
+      amount = 8;
+    }
+    setText(data.slice(0, amount));
+  }
 
-  return <h2>lorem ipsum project setup</h2>;
+  return (
+    <section className="section-center">
+      <h3>Tired of Boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraph:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-test">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
