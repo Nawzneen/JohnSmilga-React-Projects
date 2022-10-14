1fc9bf0ef2bbfdc29237c8c990117c9844ee8a0b
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-14 17:08:46 +0300

    initiating

--- 08-lorem-ipsum/setup/src/App.js
+++ 08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from "react";
+import data from "./data";
 function App() {
-  
-  
-  return (
-  <h2>lorem ipsum project setup</h2>
-    )
+  const [number, setNumber] = useState(0);
+  const [text, setText] = useState([]);
+
+  return <h2>lorem ipsum project setup</h2>;
 }
 
 export default App;
