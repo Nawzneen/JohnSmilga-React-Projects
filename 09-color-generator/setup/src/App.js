408fe1b7e52b8dcbf350ea497b0ba569d1833398
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-18 23:43:31 +0300

    minor unimportant changes

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -1,10 +1,34 @@
-import React, { useState } from 'react'
-import SingleColor from './SingleColor'
+import React, { useState } from "react";
+import SingleColor from "./SingleColor";
 
-import Values from 'values.js'
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState();
+  function handleSubmit() {}
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            id=""
+            placeholder="#a231687"
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <label htmlFor=""></label>
+          <button className="btn" type="submit">
+            submit
+          </button>
+        </form>
+      </section>
+      <section className="color"></section>
+    </>
+  );
 }
 
-export default App
+export default App;
