991f3a3851eadc7201d39732afb53672418faa56
Nazanin Nak <86608888+Nawzneen@users.noreply.github.com>
2022-10-20 00:48:03 +0300

    Almost finnishing the rgb part

--- 09-color-generator/setup/src/App.js
+++ 09-color-generator/setup/src/App.js
@@ -6,11 +6,16 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState();
+  const [list, setList] = useState([]);
   function handleSubmit(e) {
     e.preventDefault();
-    let colors = new Values(color).all(10);
-    console.log(colors);
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log("you have an error");
+    }
   }
   return (
     <>
@@ -22,6 +27,7 @@ function App() {
             value={color}
             id=""
             placeholder="#A52A2A"
+            className={`${error ? "error" : null}`}
             onChange={(e) => setColor(e.target.value)}
           />
           <label htmlFor=""></label>
@@ -30,7 +36,18 @@ function App() {
           </button>
         </form>
       </section>
-      <section className="colors">list goes here</section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              hexColor={color.hex}
+              index={index}
+            />
+          );
+        })}
+      </section>
     </>
   );
 }
